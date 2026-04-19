f274904d5f3f0f1ad1df5f6516fe15840ffc26f1
RestartGamer <ckorkmaz56@gmail.com>
2026-04-19 19:26:20 +0100

    refactor(semantics): align semantics of text elements

--- client/src/components/ui/DescriptionBlock.jsx
+++ client/src/components/ui/DescriptionBlock.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material"
 
 export function DescriptionBlock({ children, variant="bodyLarge", sx ={} }) {
     return (
-        <Typography variant={variant} sx={{
+        <Typography variant={variant} component="p" color="text.primary" sx={{
             display:"flex",
             justifyContent: "center",
             fontWeight: "300",
--- client/src/components/ui/TitleBlock.jsx
+++ client/src/components/ui/TitleBlock.jsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from "@mui/material"
-import {pageLayout} from "../../layout/layout"
+import { pageLayout } from "../../layout/layout"
 const {
-  titleSpacing
+    titleSpacing
 } = pageLayout;
 
 
@@ -12,18 +12,28 @@ export function TitleBlock({ title, children = null, variant = "heroTitle", sx =
                 alignItems: "center",
                 ...sx,
             }}>
-            <Typography variant={variant} component={
-                variant === "heroTitle" ? "h1" : variant === "headingTitle" ? "h2" : variant === "sectionTitle" && "h3" 
-            }>{title}</Typography>
+            <Typography
+                variant={variant}
+                component={
+                    variant === "heroTitle" ? "h1" : variant === "headingTitle" ? "h2" : variant === "sectionTitle" && "h3"
+                }
+                color="text.primary"
+            >
+                {title}
+            </Typography>
 
 
             {
                 children !== null &&
-                <Typography variant="sectionTitle" component="h3" sx={{
-                    fontFamily: `"EB Garamond", serif`,
-                    fontWeight: "400",
+                <Typography
+                    variant="sectionTitle"
+                    component="h3"
+                    color="text.primary"
+                    sx={{
+                        fontFamily: `"EB Garamond", serif`,
+                        fontWeight: "400",
 
-                }}>
+                    }}>
                     {children}
                 </Typography >
             }
